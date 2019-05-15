6c4e152be16c7e5d57088e3ad222f962791c4294
MinyeongKim <32475904+MinyeongKim@users.noreply.github.com>
2019-05-15 20:58:17 +0900

    Complete

--- app/src/main/java/com/example/hw3/MainActivity.java
+++ app/src/main/java/com/example/hw3/MainActivity.java
@@ -24,15 +24,11 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ArrayList<Integer> images = new ArrayList<Integer>();
-        images.add(R.drawable.dog1);
-        images.add(R.drawable.dog2);
-        images.add(R.drawable.dog3);
-
         imageView1 = (ImageView) findViewById(R.id.imageView1);
         imageView2 = (ImageView) findViewById(R.id.imageView2);
         editText = (EditText) findViewById(R.id.editText);
         button = (Button) findViewById(R.id.button);
+        //Create threads when you click the button
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -45,17 +41,48 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
-    class DogThread extends Thread{
+    public class DogThread extends Thread{
+        int imageIndex;
+        int state;
+        ArrayList<Integer> images = new ArrayList<Integer>(); //이미지들을 넣을 ArrayList
 
-        public DogThread(int num){
+        //DogThread 초기화
+        public DogThread(int idx){
+            imageIndex = idx;
 
+            //images에 dog image 넣기
+            images.add(R.drawable.dog1);
+            images.add(R.drawable.dog2);
+            images.add(R.drawable.dog3);
         }
         public void run(){
-            try{
-                Message msg = handler.obtainMessage();
-                handler.sendMessage(msg);
+            state = 0;
+            while(true){
+                //handler 객체를 사용하여 Runnable 객체를 post
+                handler.post(new Runnable(){
+                   public void run(){
+                       //Check the image Index and Load image that fits state value
+                       if(imageIndex == 0){
+                           imageView1.setImageResource(images.get(state));
+                       }
+                       else{
+                           imageView2.setImageResource(images.get(state));
+                       }
+                       //Log current status
+                       editText.append("dog #"+imageIndex+" state: "+state+"\n");
+                   }
+                });
 
-            }catch (Exception ex){}
+                try {
+                    //Setting sleep time. Minimum time = 1sec, Maximum time = 5sec
+                    int sleepTime = getRandomTime(1000, 5000);
+                    Thread.sleep(sleepTime);
+                } catch (Exception ex) {}
+
+                state++; //Change state
+                if(state == images.size()) //state가 images의 길이와 같으면, state를 0으로 초기화
+                    state = 0;
+            }
         }
 
         public int getRandomTime(int min, int max){
